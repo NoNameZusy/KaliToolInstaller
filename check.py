a3cbeba29c3069df1dcfddd08a4790a58abce790
No_Name.exe <153848626+NoNameZusy@users.noreply.github.com>
2024-07-12 17:10:33 +0400

    Update check.py

--- check.py
+++ check.py
@@ -1,8 +1,14 @@
 import os
 import time
+
+try:
+    import colorama
+except ImportError:
+    subprocess.run(["pip", "install", "colorama"], check=True)
+    import colorama
+
 from colorama import Fore, Style
 
-# Temizleme işlemi
 os.system("clear")
 
 logo = """
@@ -21,10 +27,8 @@ print(logo)
 time.sleep(2)
 
 try:
-    # Kali Linux üzerinde yüklü olan tüm paketleri al
     output = os.popen("dpkg-query -W -f='${Package}\n'").read().splitlines()
 
-    # Her bir paketi kontrol et ve durumunu yazdır
     for package in output:
         result = os.system(f"dpkg -s {package} > /dev/null 2>&1")
         if result == 0:
@@ -32,11 +36,9 @@ try:
         else:
             print(f"{package.ljust(30)}[" + Fore.RED + "Not found" + Style.RESET_ALL + "]")
         
-        # Her bir saniyede bir işlemi kontrol etmek için bekleyin
         time.sleep(1)
 
 except KeyboardInterrupt:
     print("\nExiting...")
 
-# İşlem tamamlandığında çıkış yap
 print("\nCheck completed.")
